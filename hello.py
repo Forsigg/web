8154208607af3d5d9197299af9dd15c727d66144
Forsigg <al.berhmiller@gmail.com>
2022-03-23 22:28:39 +0700

    edit hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@
 def application(env, start_response):
     status = '200 OK'
     response_headers = [
-        ('Content-Type', 'text-plain'),
+        ('Content-Type', 'text/plain'),
     ]
     body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
     start_response(status, response_headers)
