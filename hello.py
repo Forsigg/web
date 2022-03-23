08e983341f57d8ec130a66323f9689d934cde52c
Forsigg <al.berhmiller@gmail.com>
2022-03-23 22:05:59 +0700

    add hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+from os import environ
+
+
+def application(env, start_response):
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'text-plain'),
+    ]
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    start_response(status, response_headers)
+    return body
