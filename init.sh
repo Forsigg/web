aaf79012c3dc86fcc37a1e02cb13b71c8148efc3
Forsigg <al.berhmiller@gmail.com>
2022-03-23 22:51:01 +0700

    added project (Django) ask with app qa

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-
+sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:application
+sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
