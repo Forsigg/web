5eaac31b2f89de14f5f8ecbd8bf550e2e393ff07
Forsigg <al.berhmiller@gmail.com>
2022-03-24 20:48:52 +0700

    edit init.sh

--- init.sh
+++ init.sh
@@ -9,6 +9,8 @@ sudo pip3 install gunicorn
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
-sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
+PYTHONPATH='pwd'/.. gunicorn --bind 0.0.0.0:8000 ask.wsgi:application
+
+#sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
+#sudo /etc/init.d/gunicorn restart
+#sudo gunicorn -c /etc/django_conf.py ask.wsgi:application
