76bb325e598b8c1e692b34808140dc05ef224486
Forsigg <al.berhmiller@gmail.com>
2022-03-31 22:33:22 +0700

    add models for qa app (Question, Answer, QuestionManager)

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='likes_users')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User)
+
+
