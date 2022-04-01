d934f68bf8bd313a586245d4be5db52c2ebd6656
Forsigg <88813689+Forsigg@users.noreply.github.com>
2022-04-01 15:35:40 +0700

    fix init_bd

--- init_bd.sh
+++ init_bd.sh
@@ -1,3 +1,3 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database stepik_web;"
-mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+mysql -uroot -e "grant all privileges on stepik_web.* to 'box'@'localhost' with grant option;"
