2e40fa3d172c6566f6a06a1a4e187a84b1d64ce6
Forsigg <88813689+Forsigg@users.noreply.github.com>
2022-04-01 15:49:09 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,11 +3,6 @@ sudo apt install python3.5
 sudo apt install python3.5-dev
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-
-sudo apt install python3-venv
-sudo python3 -m venv venv
-source ./venv/bin/activate
-
 sudo python3 -m pip install gunicorn
 sudo python3 -m install django==2.0
 sudo python3 -m pip install mysqlclient
