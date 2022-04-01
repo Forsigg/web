1438ba8ba0570fc4f0e6e9331b1fbbd0db0bc921
Forsigg <88813689+Forsigg@users.noreply.github.com>
2022-04-01 15:38:42 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,16 @@
 sudo apt update
 sudo apt install python3.5
-sudo rm /usr/bin/python3
+sudo apt install python3.5-dev
+sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo pip3 install django==2.1
-sudo pip3 install gunicorn
 
+sudo apt install python3-venv
+sudo python3 -m venv venv
+source ./venv/bin/activate
+
+sudo python3 -m pip install gunicorn
+sudo python3 -m install django==2.0
+sudo python3 -m pip install mysqlclient
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
