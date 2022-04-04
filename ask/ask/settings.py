463e81cc7b01393569ff084233271aa108da7491
Forsigg <al.berhmiller@gmail.com>
2022-04-04 21:24:52 +0700

    add fixes

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -79,9 +79,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 sudo apt update
 sudo apt install python3.5
-sudo rm /usr/bin/python3
+sudo apt install python3.5-dev
+sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo pip3 install django==2.1
-sudo pip3 install gunicorn
-
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install django==2.0
+sudo python3 -m pip install mysqlclient
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
