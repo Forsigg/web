ca9b84160ac00b4b7bc62e28da17b20dcbee7cd9
Forsigg <al.berhmiller@gmail.com>
2022-04-05 21:57:13 +0700

    edit init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,6 @@ sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo python3 -m pip install gunicorn
 sudo python3 -m pip install django==2.0
-sudo python3 -m pip install mysqlclient
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
